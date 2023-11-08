ca3ff697c7524d548d8448434eb9c6eee2ae23cb
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-11-09 00:32:57 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -37,7 +37,7 @@ async def search(bot, message):
        _time = (int(time()) + (15*30))
        await save_dlt_message(msg, _time)
     except:
-       pass
+       await msg.delete(10)
        
 
 
